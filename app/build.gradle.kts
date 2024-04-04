7ffe827e74b78935eaa5df3c9d1343a861ba4541
Muhammad Rafi <mochrafi01@gmail.com>
2024-04-04 08:33:48 +0700

    add encryption sharedpreferences 🔒

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Jetpack Security libs
+    implementation ("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation ("com.scottyab:secure-preferences-lib:0.1.7")
 }
